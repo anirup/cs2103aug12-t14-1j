4a729d0431ca06d177b5544a5d99f4e6e8e1ca16
A0077245 <A0077245@soctf-imac-009.comp.nus.edu.sg>
2012-11-08 21:06:40 +0800

    update Event, ListOfEvent. now ListOfEvent dont show events that end before current time

---
+++ src/fileIO/FileIO.java
@@ -0,0 +1,67 @@
+package fileIO;
+ 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class FileIO {
+	private static String fileName;
+	private static BufferedReader reader;
+	private static File database;
+	private static BufferedWriter writer;
+	
+	public static ArrayList<String> setUpDatabase(String nameOfFile) throws IOException {
+		fileName = nameOfFile;
+		database = new File(fileName);
+		if (!database.exists()) {
+			database.createNewFile();
+		}
+		reader = new BufferedReader(new FileReader(fileName));
+		return retrieveDatabase();
+	}
+	
+	public static void syncToDatabase(ArrayList<String> currentListOfEvent, String nameOfFile) throws IOException {
+		fileName = nameOfFile;
+		setUpBufferedWriter();
+		Iterator<String> iterator = currentListOfEvent.iterator();
+		while(iterator.hasNext()) {
+			String content = iterator.next();
+			writeToDatabase(content);
+		}
+		closeBufferedWriter();
+		
+		return;
+	}
+	
+	public static ArrayList<String> retrieveDatabase() throws IOException {
+		ArrayList<String> listOfEvent = new ArrayList<String>();
+		String currentLine;
+		while((currentLine = reader.readLine()) != null) {
+			listOfEvent.add(currentLine);
+		}
+		
+		return listOfEvent;
+	}
+	
+	private static void closeBufferedWriter() throws IOException {
+		writer.close();
+	}
+
+	private static void setUpBufferedWriter() throws IOException {
+		writer = new BufferedWriter(new FileWriter(fileName));
+	}
+	
+	private static void writeToDatabase(String line) throws IOException {
+		writer.write(line + "\n");
+	}
+
+	public static void formatDatabase() throws IOException {
+		setUpBufferedWriter();
+		closeBufferedWriter();
+	}
+}
